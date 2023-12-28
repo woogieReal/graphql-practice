5bd6f706077cfbccf80f33f143bc45375e239597
woogieReal <woogiereal@gmail.com>
2023-12-28 14:21:39 +0900

    feat: sqlite > mysql DB 교체

--- graphql/database.py
+++ graphql/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine("sqlite:///database.sqlite3", convert_unicode=True)
+engine = create_engine("mysql+pymysql://dev:1234@mysql:3306/employees?charset=utf8mb4")
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
 )
@@ -14,26 +14,5 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    from models import Department, Employee, Role
-
-    Base.metadata.drop_all(bind=engine)
+    # Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
-
-    # Create the fixtures
-    engineering = Department(name="Engineering")
-    db_session.add(engineering)
-    hr = Department(name="Human Resources")
-    db_session.add(hr)
-
-    manager = Role(name="manager")
-    db_session.add(manager)
-    engineer = Role(name="engineer")
-    db_session.add(engineer)
-
-    peter = Employee(name="Peter", department=engineering, role=engineer)
-    db_session.add(peter)
-    roy = Employee(name="Roy", department=engineering, role=engineer)
-    db_session.add(roy)
-    tracy = Employee(name="Tracy", department=hr, role=manager)
-    db_session.add(tracy)
-    db_session.commit()
--- graphql/schema.py
+++ graphql/schema.py
@@ -1,41 +1,19 @@
-from models import Department as DepartmentModel
-from models import Employee as EmployeeModel
-from models import Role as RoleModel
+from models import Employees as EmployeesModel
 
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
 
 
-class Department(SQLAlchemyObjectType):
+class Employees(SQLAlchemyObjectType):
     class Meta:
-        model = DepartmentModel
+        model = EmployeesModel
         interfaces = (relay.Node,)
-
-
-class Employee(SQLAlchemyObjectType):
-    class Meta:
-        model = EmployeeModel
-        interfaces = (relay.Node,)
-
-
-class Role(SQLAlchemyObjectType):
-    class Meta:
-        model = RoleModel
-        interfaces = (relay.Node,)
-
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
 
-    # Allow only single column sorting
-    all_employees = SQLAlchemyConnectionField(Employee.connection, sort=Employee.sort_argument())
-
-    # Allows sorting over multiple columns, by default over the primary key
-    all_roles = SQLAlchemyConnectionField(Role.connection)
-
-    # Disable sorting over this field
-    all_departments = SQLAlchemyConnectionField(Department.connection, sort=None)
+    all_employees = SQLAlchemyConnectionField(Employees.connection)
 
 
 schema = graphene.Schema(query=Query)
