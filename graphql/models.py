37df86846f7b96a4a5869686a172afd2c517ef17
woogieReal <woogiereal@gmail.com>
2024-01-01 17:58:49 +0900

    feat: 모델 추가

--- graphql/models.py
+++ graphql/models.py
@@ -1,11 +1,44 @@
 from database import Base
-from sqlalchemy import Column, Date, ForeignKey, Integer, String
+from sqlalchemy import Column, Date, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import backref, relationship
 
-class Employees(Base):
+class Employee(Base):
     __tablename__ = "employees"
     emp_no = Column(Integer, primary_key=True)
     birth_date = Column(Date)
-    first_name = Column(String)
-    last_name = Column(String)
-    gender = Column(String)
+    first_name = Column(String(14))
+    last_name = Column(String(16))
+    gender = Column(Enum("M", "F"))
+
+class Department(Base):
+    __tablename__ = "departments"
+    dept_no = Column(String(4), primary_key=True)
+    dept_name = Column(String(40), unique_key=True)
+
+class DeptManager(Base):
+    __tablename__ = "dept_manager"
+    emp_no = Column(Integer, ForeignKey("employees.emp_no"), primary_key=True)
+    dept_no = Column(String(4), ForeignKey("departments.dept_no"), primary_key=True)
+    from_date = Column(Date)
+    to_date = Column(Date)
+
+class DeptEmp(Base):
+    __tablename__ = "dept_emp"
+    emp_no = Column(Integer, ForeignKey("employees.emp_no"), primary_key=True)
+    dept_no = Column(String(4), ForeignKey("departments.dept_no"), primary_key=True)
+    from_date = Column(Date)
+    to_date = Column(Date)
+
+class Titles(Base):
+    __tablename__ = "titles"
+    emp_no = Column(Integer, ForeignKey("employees.emp_no"), primary_key=True)
+    title = Column(String(50), primary_key=True)
+    from_date = Column(Date, primary_key=True)
+    to_date = Column(Date)
+
+class Salaries(Base):
+    __tablename__ = "salaries"
+    emp_no = Column(Integer, ForeignKey("employees.emp_no"), primary_key=True)
+    salary = Column(Integer)
+    from_date = Column(Date, primary_key=True)
+    to_date = Column(Date)
--- graphql/schema.py
+++ graphql/schema.py
@@ -1,19 +1,19 @@
-from models import Employees as EmployeesModel
+from models import Employee as EmployeeModel
 
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
 
 
-class Employees(SQLAlchemyObjectType):
+class Employee(SQLAlchemyObjectType):
     class Meta:
-        model = EmployeesModel
+        model = EmployeeModel
         interfaces = (relay.Node,)
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
 
-    all_employees = SQLAlchemyConnectionField(Employees.connection)
+    all_employees = SQLAlchemyConnectionField(Employee.connection)
 
 
 schema = graphene.Schema(query=Query)
