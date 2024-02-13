fb8b4c55b90eb4dd9c95fb8b6a3e8b17a869b45b
woogieReal <woogiereal@gmail.com>
2024-02-13 16:01:40 +0900

    feat: offset 기반 Pagination 을 위한 connection class 생성

--- graphql/schema.py
+++ graphql/schema.py
@@ -1,14 +1,27 @@
 from models import Employee as EmployeeModel
 
 import graphene
-from graphene import relay
+from graphene import relay, Connection, Int
 from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
 
+
+class ExtendedConnection(Connection):
+    class Meta:
+        abstract = True
+
+    total_count = Int()
+    edge_count = Int()
+
+    def resolve_total_count(root, info, **kwargs):
+        return root.length
+    def resolve_edge_count(root, info, **kwargs):
+        return len(root.edges)
 
 class Employee(SQLAlchemyObjectType):
     class Meta:
         model = EmployeeModel
         interfaces = (relay.Node,)
+        connection_class = ExtendedConnection
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
