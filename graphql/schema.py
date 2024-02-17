bcf1cf47e825d011f5340cb4ca0077879508ea03
woogieReal <woogiereal@gmail.com>
2024-02-17 14:01:11 +0900

    feat: artsy pagination 도입

--- graphql/schema.py
+++ graphql/schema.py
@@ -3,40 +3,19 @@ from models import Employee as EmployeeModel
 import graphene
 import graphene_sqlalchemy
 
+from relay_pagination.fields import PageConnection, PageConnectionField
 
-class ExtendedConnection(graphene.Connection):
-    class Meta:
-        abstract = True
-
-    total_count = graphene.Int()
-    edge_count = graphene.Int()
-
-    def resolve_total_count(root, info, **kwargs):
-        return root.length
-    def resolve_edge_count(root, info, **kwargs):
-        return len(root.edges)
-
-class MyConnectionField(graphene_sqlalchemy.SQLAlchemyConnectionField):
-    RELAY_ARGS = ['first', 'last', 'before', 'after']
-
-    @classmethod
-    def get_query(cls, model, info, **kwargs):
-        query = super(MyConnectionField, cls).get_query(model, info, **kwargs)
-        for field, value in kwargs.items():
-            if field not in cls.RELAY_ARGS:
-                query = query.filter(getattr(model, field) == value)
-        return query
 
 class Employee(graphene_sqlalchemy.SQLAlchemyObjectType):
     class Meta:
         model = EmployeeModel
         interfaces = (graphene.relay.Node,)
-        connection_class = ExtendedConnection
+        connection_class = PageConnection
 
 class Query(graphene.ObjectType):
     node = graphene.relay.Node.Field()
 
-    employees = MyConnectionField(
+    employees = PageConnectionField(
         Employee,
         emp_no=graphene.Int(),
     )
